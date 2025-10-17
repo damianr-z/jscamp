cc3fa21bc1559f5522293a4df028a2a073432ad8
Damian Roiz <damianx989@gmail.com>
2025-10-17 11:09:50 +0200

    aded js files to each logic

---
+++ 01-javascript/apply-button.js
@@ -0,0 +1,10 @@
+const jobsListingSection = document.querySelector('.jobs-listing');
+
+jobsListingSection.addEventListener('click', (event) => {
+  const element = event.target;
+  if (element.classList.contains('applyBtn')) {
+    element.textContent = 'Applied';
+    element.classList.add('applied');
+    element.disabled = true;
+  }
+});
---
+++ 01-javascript/main.js
@@ -0,0 +1,11 @@
+import { state } from './config.js'
+
+import './fetch-data.js'
+import './filter.js'
+import './apply-button.js'
+import './devjobs-avatar-element.js'
+
+state.count++
+
+console.log(state)
+
