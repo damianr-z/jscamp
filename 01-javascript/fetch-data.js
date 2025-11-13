3bdc1e3d73c0f48a0fb803f935856b4e34753dd1
Damian Roiz <damianx989@gmail.com>
2025-11-13 10:17:14 +0100

    chore: reset 01-javascript to upstream/main

--- 01-javascript/fetch-data.js
+++ 01-javascript/fetch-data.js
@@ -1,51 +1,27 @@
-const container = document.querySelector('.jobs-listings');
+const container = document.querySelector('.jobs-listings')
 
-const RESULTS_PER_PAGE = 3;
+const RESULTS_PER_PAGE = 3
 
-fetch('./data.json') /* fetch es asíncrono */
+fetch("./data.json") /* fetch es asíncrono */
   .then((response) => {
     return response.json();
   })
   .then((jobs) => {
+    jobs.forEach(job => {
+      const article = document.createElement('article')
+      article.className = 'job-listing-card'
+      
+      article.dataset.modalidad = job.data.modalidad
+      article.dataset.nivel = job.data.nivel
+      article.dataset.technology = job.data.technology
 
-    jobs.forEach((job) => {
-      const article = document.createElement('article');
-      article.className = 'job-listing-card';
-
-      article.dataset.technology = job.data.technology;
-      article.dataset.modalidad = job.data.modalidad;
-      article.dataset.nivel = job.data.nivel;
-
-      article.innerHTML = `<section class="jobHeader">
-        <div>
+      article.innerHTML = `<div>
           <h3>${job.titulo}</h3>
-          <p>${job.empresa} | ${job.ubicacion}</p>
-          </div>
-          <button class="button-apply-job">Aplicar</button>
-          </section>
-          <p>${job.descripcion}</p>`;
+          <small>${job.empresa} | ${job.ubicacion}</small>
+          <p>${job.descripcion}</p>
+        </div>
+        <button class="button-apply-job">Aplicar</button>`
 
-          console.log(article);
-
-      container.appendChild(article);
-    });
-
+      container.appendChild(article)
+    })
   });
-
-// <article data-experience="junior" class="jobPost">
-//   <section class="jobHeader">
-//     <div>
-//       <h3>Software Engineer</h3>
-//       <p>Tech Solutions Inc | Hybrid</p>
-//     </div>
-//     <button class="applyBtn">Apply</button>
-//   </section>
-//   <p>
-//     We are looking for a Software Engineer with experience in web
-//     development and knowledge of JavaScript, React, and Node.js. The
-//     ideal candidate should be able to work in a team environment and
-//     have good communication skills.
-//   </p>
-// </article>
-///
-
--- 01-javascript/main.js
+++ 01-javascript/main.js
@@ -1,3 +1,10 @@
-import './fetch-data.js';
-import './applyBtn.js';
-import './filterJobs.js';
+import { state } from './config.js'
+
+import './fetch-data.js'
+import './filters.js'
+import './apply-button.js'
+import './devjobs-avatar-element.js'
+
+state.count++
+
+console.log(state)
