39f04a4f70bd0100a3d194d71a19bedc77b23aa2
Damian Roiz <damianx989@gmail.com>
2026-04-12 14:33:10 -0400

    Add logging middleware to track request method and URL

--- 04-express/index.js
+++ 04-express/index.js
@@ -3,10 +3,18 @@ import express from 'express';
 const PORT = process.env.PORT ?? 1234;
 const app = express();
 
-app.get('/', (req, response) => {
-  response.send('Hello Word');
+app.use((req, res, next) => {
+  const timeString = new Date().toLocaleTimeString();
+  console.log(`[${timeString}] ${req.method} ${req.url}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Word');
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor levantado en http://localhost:${PORT}`);
 });
+
+// un middleware es una funcion que se ejecuta antes de que llegue a una ruta.
